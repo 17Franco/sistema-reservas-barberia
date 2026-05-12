d3684581460b954137f080b7b50f610b2b14ef41
Franco Echaide <130937215+17Franco@users.noreply.github.com>
2026-05-12 16:30:48 -0300

    quito archivo de Parametros de Conexion

--- ProyectoBarberiaBackend/src/infraestructura/config/ParametrosConexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-    
-//defino constantes a ser utilizadas.
-    define('SERVIDOR','localhost');
-    define('USUARIO','admin');
-    define('CONTRASEÑA','admin');
-    define('BASEDATOS','barberia');
-?>
