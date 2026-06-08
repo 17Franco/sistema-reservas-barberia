48594102111d98e2c37123df584ea51460c91fce
Juanpi <juanpifontes28@gmail.com>
2026-06-08 19:49:43 -0300

    Separe Home como layout y agregue pantalla principal, con sus respectivas rutas etc...

--- BarberiaFrontend/src/app/app.routes.ts
+++ BarberiaFrontend/src/app/app.routes.ts
@@ -4,18 +4,33 @@ import { Home } from './pages/home/home';
 import { authGuard } from './guards/auth-guard';
 import { Servicios } from './pages/servicios/servicios';
 import { PaginaPerfil } from './pages/pagina-perfil/pagina-perfil';
+import { PantallaPrincipal } from './pages/pantalla-principal/pantalla-principal'; // importamos la pantalla principal para agregarla a las rutas.
 
 //aca agregamos el path para que si en el navegador busca home redirija a page home o login etc
 //cada pagina debe tener una ruta
 export const routes: Routes = [
   {
-    path: '', component: Home, canActivate: [authGuard], children:[
-      { path: 'MiPerfil', component: PaginaPerfil }
-    ]},
-  {
-    path: 'servicios', component: Servicios, canActivate: [authGuard]
+    path: '',
+    component: Home,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        component: PantallaPrincipal
+      },
+      {
+        path: 'servicios',
+        component: Servicios
+      },
+      {
+        path: 'MiPerfil',
+        component: PaginaPerfil
+      }
+    ]
   },
   {
-    path: 'auth',component: LoginRegistro
+    path: 'auth',
+    component: LoginRegistro
   },
+
 ];
--- BarberiaFrontend/src/app/pages/home/home.ts
+++ BarberiaFrontend/src/app/pages/home/home.ts
@@ -11,42 +11,6 @@ import { Auth } from '../../services/auth';
   styleUrl: './home.scss',
 })
 export class Home {
-  authService = inject(Auth);
-  router = inject(Router);
-  cd = inject(ChangeDetectorRef);
-  serviciosDestacados: any[] = [];
-  cargando = true;
-  error: string | null = null;
 
-  constructor() {
-    this.loadServicios();
-  }
-
-  get mostrarHome(): boolean {
-    const url = this.router.url.split('?')[0].split('#')[0];
-    return url === '/';
-  }
-
-  loadServicios(): void {
-    console.log('Home.loadServicios iniciada');
-    this.cargando = true;
-    this.error = null;
-
-    this.authService.getServicios().subscribe({
-      next: (res: any) => {
-        console.log('Home.getServicios.next', res);
-        const servicios = res.servicios || [];
-        this.serviciosDestacados = servicios.slice(0, 3);
-        this.cargando = false;
-        console.log('Home.serviciosDestacados', this.serviciosDestacados, 'cargando=', this.cargando);
-        this.cd.detectChanges();
-      },
-      error: (err) => {
-        console.error('Servicio /servicios error:', err);
-        this.error = 'No se pudieron cargar los servicios destacados. Verifica el backend o la conexión.';
-        this.cargando = false;
-        this.cd.detectChanges();
-      },
-    });
-  }
+  
 }
