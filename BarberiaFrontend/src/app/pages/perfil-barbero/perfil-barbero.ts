f48180eca05bfaab3f3e9786d5dd2cb846877d6a
SantiGDL <santiagoguadalupedeleon@gmail.com>
2026-06-21 19:20:02 -0300

    Mismo cambio que en el usuario aplicado en el barbero (Reservas Canceladas al historial)

--- BarberiaFrontend/src/app/pages/perfil-barbero/perfil-barbero.ts
+++ BarberiaFrontend/src/app/pages/perfil-barbero/perfil-barbero.ts
@@ -123,11 +123,11 @@ export class PerfilBarbero implements OnInit{
         ).length;
 
         this.reservasAsociadas = reservas.filter(
-          (reserva: ReservaInterface) => reserva.estado !== 'COMPLETADA',
+          (reserva: ReservaInterface) => reserva.estado == 'PENDIENTE' || reserva.estado == 'CONFIRMADA',
         );
 
         this.historialReservas = reservas.filter(
-          (reserva: ReservaInterface) => reserva.estado === 'COMPLETADA',
+          (reserva: ReservaInterface) => reserva.estado === 'COMPLETADA' || reserva.estado == 'CANCELADA',
         );
       }
     } catch (error) {
