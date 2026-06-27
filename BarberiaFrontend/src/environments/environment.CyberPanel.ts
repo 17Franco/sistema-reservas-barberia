0052c8eaa8e0151a0841d5b8b6bb02cd270e000f
17Franco <echaidefranco@gmail.com>
2026-06-27 12:17:29 -0300

    eviroment para cyberPanel

---
+++ BarberiaFrontend/src/environments/environment.CyberPanel.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://sitio.tec.cuatro/ProyectoBarberiaBackend/public/index.php',
+  backendPublicUrl: 'https://sitio.tec.cuatro/ProyectoBarberiaBackend/public'
+};
